a266300a4830733636792becfe972139258278a9
alihaij <a3haij@gmail.com>
2022-11-19 12:41:41 +0300

    last commit

--- lib/model/seller.dart
+++ lib/model/seller.dart
@@ -2,8 +2,10 @@ class Supplier {
   final String name;
   final String address;
   final String webSite;
+  final String vatRegistrationNum;
 
   const Supplier({
+    required this.vatRegistrationNum,
     required this.name,
     required this.address,
     required this.webSite,
